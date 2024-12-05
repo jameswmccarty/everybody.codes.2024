cd8c1c13a14129b8da592e96d393bf3a09e2cc6e
jameswmccarty <jameswmccarty@gmail.com>
2024-12-04 20:22:13 -0500

    Add files via upload

---
+++ q3p3.py
@@ -0,0 +1,27 @@
+grid = dict()
+
+with open('everybody_codes_e2024_q03_p3.txt','r') as infile:
+	y = 0
+	for line in infile:
+		for x, char in enumerate(line.strip()):
+			if char == '.':
+				grid[(x,y)] = 0
+			else:
+				grid[(x,y)] = 1
+		y += 1
+
+while True:
+	new_grid = dict()
+	dug = False
+	for k,v in grid.items():
+		x,y = k
+		if v > 0 and all(((x+dx,y+dy) in grid and grid[(x+dx,y+dy)] == v) for dx,dy in ((0,1),(0,-1),(1,0),(-1,0),(-1,-1),(1,1),(1,-1),(-1,1))):
+			new_grid[(x,y)] = v+1
+			dug = True
+		else:
+			new_grid[(x,y)] = v
+	grid = new_grid
+	if not dug:
+		break
+
+print(sum(v for v in grid.values()))
